0f4641755ea84a4e9b66f3ac34e9ff528a55261e
Aurélien Lair <aurelien.lair@musement.com>
2022-09-23 15:32:47 +0200

    NO_TICKET | feat: add new command to run the test with NPM + fix test

--- search/pages/search.js
+++ search/pages/search.js
@@ -1,27 +1,31 @@
-import { Selector, t } from 'testcafe'
+import { Selector, t } from "testcafe";
 import {
   SEARCH_INPUT_BUTTON,
   SEARCH_CRITERIA_VALUE,
   SUBMIT_BUTTON,
   FIRST_CITE_ELEMENT,
-  FIRST_CITE_ELEMENT_EXPECTED_VALUE
-} from '../../object-repository/properties'
+  FIRST_CITE_ELEMENT_EXPECTED_VALUE,
+  REJECT_ALL_BUTTON,
+} from "../../object-repository/properties";
 
 export default class SearchPage {
-  constructor () {
-    this.search_input_button = Selector(SEARCH_INPUT_BUTTON)
-    this.submit_button = Selector(SUBMIT_BUTTON)
+  constructor() {
+    this.reject_all_button = Selector(REJECT_ALL_BUTTON);
+    this.search_input_button = Selector(SEARCH_INPUT_BUTTON);
+    this.submit_button = Selector(SUBMIT_BUTTON);
   }
 
   async insertCriteriaAndSearch() {
-    await t.expect(this.search_input_button.exists).ok()
+    await t.click(this.reject_all_button);
+    await t.expect(this.search_input_button.exists).ok();
     await t
       .typeText(this.search_input_button, SEARCH_CRITERIA_VALUE)
-      .pressKey('enter')
+      .pressKey("enter");
   }
 
   async ensureFirstResultIsRelevantWithSearchCriteria() {
-    await t.expect(Selector(FIRST_CITE_ELEMENT).innerText)
-      .eql(FIRST_CITE_ELEMENT_EXPECTED_VALUE)
+    await t
+      .expect(Selector(FIRST_CITE_ELEMENT).innerText)
+      .eql(FIRST_CITE_ELEMENT_EXPECTED_VALUE);
   }
 }
